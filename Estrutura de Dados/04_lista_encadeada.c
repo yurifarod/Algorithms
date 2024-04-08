cce277bf79d1728475d883b2a46b93ffdf2ce516
Yúri Faro <yurifarod@yahoo.com.br>
2024-04-08 16:27:05 -0300

    Add files via upload
    Codigo de lista encadeada completo!

--- Estrutura de Dados/04_lista_encadeada.c
+++ Estrutura de Dados/04_lista_encadeada.c
@@ -23,6 +23,10 @@ typedef struct tipo_aluno {
 	struct tipo_aluno *prox;
 } Aluno;
 
+void iniciar(Aluno **cabeca){
+	*cabeca = NULL;
+}
+
 void insere(char nome[100], char curso[100], int matricula, Aluno **cabeca){
 	if(*cabeca == NULL){
 		*cabeca = (Aluno *) malloc(sizeof(Aluno));
@@ -40,11 +44,11 @@ void insere(char nome[100], char curso[100], int matricula, Aluno **cabeca){
 	  	novo->prox = (*cabeca)->prox;
 	  	(*cabeca)->prox = novo;
   	}
-  	printf("%d Aluno teve sua insercao concluida!\n", matricula);
+  	printf("%d - Aluno teve sua insercao concluida!\n", matricula);
 }
 
 void imprime(Aluno **cabeca){
-	printf("========= RELATORIO DE ALUNOS =========\n");
+	printf("\n\n========= RELATORIO DE ALUNOS =========\n");
 	Aluno *p = (*cabeca);
 	while(p != NULL){
 		printf("%d\n", p->matricula);
@@ -55,9 +59,85 @@ void imprime(Aluno **cabeca){
 	}
 }
 
+Aluno* buscar(int matricula, Aluno **cabeca){
+	Aluno *p = (*cabeca);
+
+	if(p == NULL){
+		return NULL;
+	}
+
+	while(p != NULL){
+		if(p->matricula == matricula){
+			printf("### Aluno encontrado ###\n");
+			printf("%d\n", p->matricula);
+			printf("%s\n", p->nome);
+			printf("%s\n", p->curso);
+			return p;
+			break;
+		}
+		p = p->prox;
+	}
+
+	return NULL;
+}
+
+/*Esse metodo foi desenvolvido para dar clareza a busca e ao entendimento do codigo*/
+Aluno* buscar_anterior(int matricula, Aluno **cabeca){
+	Aluno *p = (*cabeca);
+	Aluno *ant = NULL;
+
+	if(p == NULL){
+		return NULL;
+	}
+
+	while(p != NULL){
+		if(p->matricula == matricula){
+			printf("### Aluno encontrado ###\n");
+			printf("%d\n", ant->matricula);
+			printf("%s\n", ant->nome);
+			printf("%s\n", ant->curso);
+			return ant;
+			break;
+		}
+		ant = p;
+		p = p->prox;
+	}
+
+	return NULL;
+}
+
+int remover_elemento(int matricula, Aluno **cabeca){
+
+	Aluno *anterior = buscar_anterior(matricula, cabeca);
+	Aluno *delete = buscar(matricula, cabeca);
+
+	// se for NULL, é porque não existe, então retorna 0
+	if(delete == NULL){
+		printf("Aluno nao cadastrado na base!\n");
+		return 0;
+	}
+	if(anterior != NULL){
+		anterior->prox = delete->prox;
+	}
+	if(delete == (*cabeca) ){
+		(*cabeca) = delete->prox;
+	}
+
+	free(delete);
+	delete = NULL;
+
+	printf("Matricula - %d, removida com sucesso\n", matricula);
+
+	return 1;
+}
+
+
+
 int main() { 
 	
-	Aluno *cabeca = NULL;
+	Aluno *cabeca;
+
+	iniciar(&cabeca);
 
 	insere("Joao dos Santos", "Analise e Desenvolvimento de Sistemas", 2023001, &cabeca);
 	insere("Jose dos Almeida", "Analise e Desenvolvimento de Sistemas", 2023002, &cabeca);
@@ -65,6 +145,13 @@ int main() {
 	insere("Sonia de Jesus", "Analise e Desenvolvimento de Sistemas", 2023004, &cabeca);
 
 	imprime(&cabeca);
-	
+
+	buscar(2023004, &cabeca);
+	buscar_anterior(2023004, &cabeca);
+
+	remover_elemento(2023004, &cabeca);
+
+	imprime(&cabeca);
+
 	return 0;
 }
