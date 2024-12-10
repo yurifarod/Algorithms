10e403a48de2a94fc6fa0120a648be0414f36be7
Yúri Faro <yurifarod@yahoo.com.br>
2024-12-10 12:31:29 -0300

    Update request_api_telegram.py

--- RaspberryPI/request_api_telegram.py
+++ RaspberryPI/request_api_telegram.py
@@ -2,7 +2,7 @@
 
 import requests
 
-response = requests.get(url="http://192.168.131.102:8087")
+response = requests.get(url="http://ip_of_nifi_host:8087")
 
 #import os
 #os.system('curl -v http://192.168.15.9:8087')
