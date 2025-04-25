07af8666fb47120c24691d9b86a71bfb098984a2
yfdantas <yfdantas@sefaz.se.gov.br>
2025-04-25 11:27:08 -0300

    Alteracoes de diretorio

--- Codigos Auxiliares/consulta_volume_tabelas.sql
+++
@@ -1,8 +0,0 @@
-/*Consulta que retorna as tabelas com o maior volume de dados (em MB) para Oracle*/
-
-select lower(SEGMENT_NAME) as "Nome da Tabela",
-		BYTES/1024/1024 as "Tamanho em MB",
-		tablespace_name as "Nome da Tablespace"
-from dba_segments
-where segment_type = 'TABLE'
-order by BYTES desc
