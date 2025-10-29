6d5fb3adac6fa22f0eac6a102e0f746c90ff202d
Yúri Faro <yurifarod@yahoo.com.br>
2025-10-29 12:12:31 -0300

    Update matar_processos_no_greenplum.sql

--- Banco de Dados/matar_processos_no_greenplum.sql
+++ Banco de Dados/matar_processos_no_greenplum.sql
@@ -1,3 +1,15 @@
+/*Consultar dados sobre sessao*/
+select pid as id,
+		usename,
+		application_name,
+		client_addr,
+		now() - query_start as tempo_execucao,
+		state
+from pg_stat_activity
+where pid <> pg_backend_pid()
+		and datname = 'nome_database';
+
+/*Matar sessao*/
 select pg_terminate_backend(pid) 
 from pg_stat_activity 
 WHERE pid <> pg_backend_pid() --nao matar minha propria sessao
