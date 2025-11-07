ea6c6ce3374ca8b1406f7381e0c33bbd335660a3
Yúri Faro <yurifarod@yahoo.com.br>
2025-11-07 10:15:43 -0300

    Update dba_query_greenplum.sql

--- Banco de Dados/dba_query_greenplum.sql
+++ Banco de Dados/dba_query_greenplum.sql
@@ -14,3 +14,25 @@ select pg_terminate_backend(pid)
 from pg_stat_activity 
 WHERE pid <> pg_backend_pid() --nao matar minha propria sessao
 		AND datname = 'nome_database';
+
+
+/*Dez maiores taelas*/
+WITH table_stats AS (
+	select schemaname,
+			tablename,
+			pg_relation_size(schemaname || '.'|| tablename) as table_size,
+			(pg_total_relation_size(schemaname || '.'|| tablename) - pg_relation_size(schemaname || '.'|| tablename)) as index_size,
+			pg_total_relation_size(schemaname || '.'|| tablename) as total_size
+	from pg_tables
+)
+select table_stats.schemaname,
+		table_stats.tablename,
+		pg_size_pretty(table_stats.table_size) as table_size,
+		pg_size_pretty(table_stats.index_size) as index_size,
+		pg_size_pretty(table_stats.total_size) as total_size
+from table_stats
+where table_stats.schemaname = 'public'
+order by table_stats.total_size desc,
+		table_stats.index_size desc,
+		table_stats.table_size desc
+limit 10;
