6180a2724735c80610d8cee4efa948f2b269c984
Yúri Faro <yurifarod@yahoo.com.br>
2026-05-19 20:29:51 -0300

    Alterar o Cliente Repository

--- Java WEB/Spring/ClienteRepository.java
+++ Java WEB/Spring/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import com.example.demo.model.Cliente;
+import jakarta.transaction.Transactional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-	
+
+	@Transactional
+	@Modifying
 	void deleteByCpf(String cpf);
 }
